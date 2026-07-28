a88564485f177b444ccd04a93e09ef36b84ea2d8
Jerimy2021 <jerimy.sandoval@utec.edu.pe>
2026-07-28 14:19:20 -0500

    feat: nixos update telescope lua treestriter

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,3 +1,10 @@
+local ok, parsers = pcall(require, "nvim-treesitter.parsers")
+if ok and not parsers.ft_to_lang then
+  parsers.ft_to_lang = function(ft)
+    return vim.treesitter.language.get_lang(ft) or ft
+  end
+end
+
 local telescope = require("telescope")
 local builtin = require("telescope.builtin")
 
@@ -16,6 +23,4 @@ vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope: Busca
 vim.keymap.set("n", "<leader>fg", builtin.live_grep,   { desc = "Telescope: Buscar texto (grep)" })
 vim.keymap.set("n", "<leader>fb", builtin.buffers,     { desc = "Telescope: Buffers abiertos" })
 vim.keymap.set("n", "<leader>fh", builtin.help_tags,   { desc = "Telescope: Ayuda" })
-
 vim.keymap.set("n", "<leader>j", "<cmd>Telescope jumplist<CR>", { desc = "Jump list" })
-
